855b66fea9ade74f18c31f53570696dced6e5253
codennis09 <dennis.demelolima@gmail.com>
2024-11-28 19:14:48 -0300

    ajuste no codigo

--- idade.py
+++ idade.py
@@ -2,6 +2,6 @@ nome = input("Digite o seu nome: ")
 idade = int(input("Digite a sua idade: "))
 
 if idade >= 18:
-    print(f"Olá {nome}, Seja bem vindo(a)!!!")
+    print(f"Olá {nome}, Seja bem vindo(a)")
 else:
     print(f"Olá {nome}, você ainda é menor de idade! Tente novamente mais tarde")
